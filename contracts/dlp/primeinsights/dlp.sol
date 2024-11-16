92bae1db97ee6d4db73f1601bf7239fe52cf134b
germ3n <gasfggit@proton.me>
2024-11-16 23:14:54 +0100

    fix inheritance

--- contracts/dlp/primeinsights/dlp.sol
+++ contracts/dlp/primeinsights/dlp.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.24;
 
-import { Rewards }      from "./rewards.sol";
-import { Permissions }  from "./permissions.sol";
-abstract contract DLP is Permissions, Rewards 
+import { Common }           from "./common.sol";
+import { Rewards }          from "./rewards.sol";
+import { Contributions }    from "./contributions.sol";
+import { Permissions }      from "./permissions.sol";
+abstract contract DLP is Common, Permissions, Contributions, Rewards
 {
 }
