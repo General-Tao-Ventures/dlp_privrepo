775382c41de9f9cc44102a8a7ebe7735e319e276
germ3n <gasfggit@proton.me>
2024-12-06 17:02:18 +0000

    make dlp not abstract

--- contracts/dlp/primeinsights/dlp.sol
+++ contracts/dlp/primeinsights/dlp.sol
@@ -1,17 +1,63 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.24;
 
-import { Common }           from "./common.sol";
-import { Rewards }          from "./rewards.sol";
-import { Contributions }    from "./contributions.sol";
-import { Permissions }      from "./permissions.sol";
-import { DLPInterface }     from "./interface.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+import { Common }                               from "./common.sol";
+import { Rewards }                              from "./rewards.sol";
+import { Contributions }                        from "./contributions.sol";
+import { Permissions }                          from "./permissions.sol";
+import { DLPInterface }                         from "./interface.sol";
+import { TEEPool }                              from "./tee.sol";
+import { ITeePool }                             from "../../dependencies/teePool/interfaces/ITeePool.sol";
+import { IDataRegistry }                        from "../../dependencies/dataRegistry/interfaces/IDataRegistry.sol";
+import { DataLiquidityPoolImplementation }      from "../DataLiquidityPoolImplementation.sol";
 
 uint128 constant PERMISSION_FINISH_EPOCH            = 0x400;
 uint128 constant PERMISSION_SET_NATIVE_REWARD_TOKEN = 0x800;
 
-abstract contract DLP is Permissions, Common, Contributions, Rewards, DLPInterface
+contract DLP is Permissions, Common, Contributions, Rewards, DLPInterface, TEEPool,
+    UUPSUpgradeable,
+    PausableUpgradeable,
+    Ownable2StepUpgradeable,
+    ReentrancyGuardUpgradeable,
+    MulticallUpgradeable
 {
+    function initialize(
+        DataLiquidityPoolImplementation.InitParams memory params
+    ) external initializer
+    {
+        __Ownable2Step_init();
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
+        __Pausable_init();
+
+        //name = params.name;
+        _dataRegistry = IDataRegistry(params.dataRegistryAddress);
+        _nativeRewardToken = params.tokenAddress;
+        _teePool = ITeePool(params.teePoolAddress);
+        //publicKey = params.publicKey;
+        //proofInstruction = params.proofInstruction;
+        //fileRewardFactor = params.fileRewardFactor;
+
+        _transferOwnership(params.ownerAddress);
+        _superadminAddress = params.ownerAddress;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual override onlyOwner {}
+
+    constructor() 
+    {
+        _disableInitializers();
+    }
+
     function _finishEpoch() internal
     {
         require(!_paused, "Contract is paused");
