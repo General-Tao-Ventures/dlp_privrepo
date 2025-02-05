7b01c548672ad0a763b370058fe7ccebd4168e26
ronx-labs <ron.sam624@gmail.com>
2025-02-04 21:36:39 -0500

    feat: script for finalize epoch

---
+++ scripts/finalize-epoch.ts
@@ -0,0 +1,47 @@
+import { ethers } from "hardhat";
+
+const implementationContractName = "DLP";
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    const dlpAddress = process.env.DLP_PROXY_ADDRESS as string;
+
+    const dlp = await ethers.getContractAt(
+        implementationContractName,
+        dlpAddress,
+    );
+
+    // pause the dlp
+    const txPause = await dlp.connect(deployer).pause();
+    await txPause.wait();
+    console.log("===> Paused the DLP");
+
+    const epoch = await dlp.getCurrentEpoch();
+
+    // update scores
+    const contributorsCount = await dlp.getNumContributors();
+    console.log(`===> Updating scores for ${contributorsCount} contributors`);
+
+    for (let i = 0; i < contributorsCount; i++) {
+        const txUpdateScore = await dlp.connect(deployer).updateScoreForContributior(i, epoch);
+        await txUpdateScore.wait();
+        console.log(`===> Updated score for contributor: ${i}`);
+    }
+
+    // unpause the dlp
+    const txUnpause = await dlp.connect(deployer).unpause();
+    await txUnpause.wait();
+    console.log("===> Unpaused the DLP");
+
+    // finish epoch
+    const txFinishEpoch = await dlp.connect(deployer).finishEpoch();
+    await txFinishEpoch.wait();
+    console.log("===> Finished the epoch");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
