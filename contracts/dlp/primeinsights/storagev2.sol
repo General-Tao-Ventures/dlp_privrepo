c5517a8b18598b9c3be2ca9a9190bdcd73bc5737
ronx-labs <ron.sam624@gmail.com>
2025-03-21 08:38:26 -0400

    temp: remove one variable for upgradability

--- contracts/dlp/primeinsights/storagev2.sol
+++ contracts/dlp/primeinsights/storagev2.sol
@@ -109,8 +109,8 @@ abstract contract StorageV2
     // maxClaimableEpoch
     uint64 internal _maxClaimableEpoch;
 
-    // first epoch to recycle for contributor
-    mapping(uint256 contributor => uint64 epoch) internal _firstEpochToRecycleForContributor;
+    // // first epoch to recycle for contributor
+    // mapping(uint256 contributor => uint64 epoch) internal _firstEpochToRecycleForContributor;
 
     //gap
     uint256[46] private __gap;
